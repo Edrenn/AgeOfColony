01d619058082a4de7943e89defaff62d54a0701b
Edrenn <edern.rodriguez@orange.fr>
2018-07-25 11:15:57 +0200

    [feat] Rework header

--- AgeOfColony/AgeOfColony/Models/Resource.cs
+++ AgeOfColony/AgeOfColony/Models/Resource.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -11,6 +12,7 @@ namespace AgeOfColony.Models
         public String Name { get; set; }
         public int RarePercentage { get; set; }
 
+        [ForeignKey("RareVersion")]
         public int RareResourceId { get; set; }
         public RareResource RareVersion { get; set; }
     }
